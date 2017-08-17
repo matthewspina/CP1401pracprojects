f590bf958b8ce22c6eb864159b9b3f23442e7bf2
matthewspina <matthew.spina@my.jcu.edu.au>
2017-08-17 12:10:53 +1000

    Add functions to get name and the number of letters it will print

--- prac_03/name_letters.py
+++ prac_03/name_letters.py
@@ -1,4 +1,20 @@
-name = str(input("name?"))
-if len(name)==0:
-    print("name can not be blank")
-print(name[::2])
+def main():
+    name = get_name()
+    number = int(input("how many letters?"))
+    print_name(name, number)
+
+
+def get_name():
+    name = str(input("name?"))
+    while len(name) == 0:
+        print("name can not be blank")
+        name = str(input("name?"))
+    return name
+
+
+
+def print_name(name, number):
+    print(name[::number])
+
+
+main()
