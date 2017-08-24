3aa20b436806a2239b2ac50ac413c228c7175f6c
matthewspina <matthew.spina@my.jcu.edu.au>
2017-08-24 12:31:18 +1000

    Add ticket generator file and added code to print the required tickets with 6 random numbers

---
+++ prac_04/ticket_generator.py
@@ -0,0 +1,12 @@
+import random
+
+pick_number = int(input("How many quick picks?"))
+for i in range(pick_number):
+    quick_pick = []
+    for n in range(6):
+        number = (random.randint(1,45))
+        quick_pick.append(number)
+    quick_pick.sort()
+    print (quick_pick)
+
+
