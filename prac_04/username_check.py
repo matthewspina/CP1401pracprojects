02deb3b0dc4e1979bd494c20f2a9efca358661e2
matthewspina <matthew.spina@my.jcu.edu.au>
2017-08-24 11:49:14 +1000

    Add username_check file and add code to print if the username is in or not in the list

---
+++ prac_04/username_check.py
@@ -0,0 +1,10 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+'swei45''BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = str(input("Username:"))
+if username in usernames:
+    print("access granted")
+else:
+    print("access denied")
+
+
