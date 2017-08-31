a3efb1171b181e0d1255032896e2c63aad5d8cb1
matthewspina <matthew.spina@my.jcu.edu.au>
2017-08-31 11:37:28 +1000

    Reformat code

---
+++ prac_05/colour_codes.py
@@ -0,0 +1,13 @@
+COLOUR_NAMES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb",
+                "AntiqueWhite2": "#eedfcc", "aquamarine1": "7fffd4", "azure1": "#f0ffff", "beige": "#f5f5dc",
+                "black": "#000000", "blue1": "#0000ff"}
+
+colour = str(input("Enter colour:"))
+while colour != "":
+    if colour in COLOUR_NAMES:
+        print (colour, "is",COLOUR_NAMES[colour])
+    else:
+        print("Invalid colour")
+        colour = str(input("Enter colour:"))
+
+
--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,7 +6,9 @@ File needs reformatting
 
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-# print(STATE_NAMES)
+
+for k, v in STATE_NAMES.items():
+    print("{:<4s} is {:<1s}".format(k,v))
 
 state = input("Enter short state: ").upper()
 while state != "":
@@ -15,8 +17,6 @@ while state != "":
     else:
         print("Invalid short state")
     state = input("Enter short state: ").upper()
-i = 0
-for k, v in STATE_NAMES.items():
-    print("{:<4s} is {:<1s}".format(k,v))
+
 
 
