071c183247d63836f131a2b53973ef05159ea9f4
matthewspina <matthew.spina@my.jcu.edu.au>
2017-10-05 11:47:21 +1000

    made the test file and edited the taxi file

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,14 @@
+
+from prac_08.taxi import Taxi
+
+def main():
+
+    taxi1 = Taxi("Prius 1",100)
+    print (taxi1)
+    taxi1.drive(40)
+    print(taxi1,taxi1.get_fare())
+    taxi1.start_fare()
+    taxi1.drive(100)
+    print(taxi1,taxi1.get_fare())
+
+main()
